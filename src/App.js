e82a3664cda5beb698298074c88a62fc9a829cd8
Harshtyagi19 <harshtyagi9719@gmail.com>
2023-05-11 17:28:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/Navbar';
 import News from './components/News';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
 
 export default class App extends Component {
-
+   pageSize=6;
   render() {
     return (
       <div>
-        <NavBar/>
-        <News pageSize={6} country="in" category="general"/>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country='in' category='general' />}></Route>
+            <Route exact path='/bininess' element={<News key='bininess' pageSize={this.pageSize} country='in' category='bininess' />}></Route>
+            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' />}></Route>
+            <Route exact path='/health' element={<News key='health' pageSize={this.pageSize} country='in' category='health' />}></Route>
+            <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country='in' category='science' />}></Route>
+            <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country='in' category='sports' />}></Route>
+            <Route exact path='/technology' element={<News key='technology' pageSize={this.pageSize} country='in' category='technology' />}></Route>
+          </Routes>
+        </Router>
       </div>
     )
   }
