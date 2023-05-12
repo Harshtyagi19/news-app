bacdcc8df7be56ef4bb57e0ef478035aac0635ee
Harshtyagi19 <harshtyagi9719@gmail.com>
2023-05-12 11:10:51 +0530

    making navbaf fixed on top

--- src/components/News.js
+++ src/components/News.js
@@ -60,7 +60,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsApp - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '80px' }}>NewsApp - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
